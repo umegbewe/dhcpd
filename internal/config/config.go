1e224c957911e471f5f3fb973d08549936026092
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2025-03-04 23:42:05 +0100

    refactor unit tests
    Signed-off-by: Umegbewe Nwebedu <nwebedujunior55@gmail.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,39 +9,51 @@ import (
 )
 
 type Config struct {
-	Server struct {
-		IPStart                string   `yaml:"ip_start"`
-		IPEnd                  string   `yaml:"ip_end"`
-		SubnetMask             string   `yaml:"subnet_mask"`
-		LeaseTime              int      `yaml:"lease_time"`
-		Gateway                string   `yaml:"gateway"`
-		ServerIP               string   `yaml:"server_ip"`
-		DNSServers             []string `yaml:"dns_servers"`
-		TFTPServerName         string   `yaml:"tftp_server_name"`
-		BootFileName           string   `yaml:"boot_file_name"`
-		Interface              string   `yaml:"interface"`
-		Port                   int      `yaml:"port" default:"67"`
-		CleanupExpiredInterval int      `yaml:"cleanup_expired_interval" default:"120"`
-		ARPCheck               bool     `yaml:"arp_check" default:"true"`
-	} `yaml:"server"`
-	Logging struct {
-		Level string `yaml:"level"`
-	} `yaml:"logging"`
-	Metrics struct {
-		Enabled       bool   `yaml:"enabled" default:"true"`
-		ListenAddress string `yaml:"listen_address" default:":9100"`
-	} `yaml:"metrics"`
-	Database struct {
-		Type string `yaml:"type" default:"bolt"`
-		Bolt struct {
-			Path string `yaml:"path"`
-		} `yaml:"bolt"`
-		Redis struct {
-			Addr     string `yaml:"addr" default:"localhost:6379"`
-			Password string `yaml:"password"`
-			DB       int    `yaml:"db" default:"0"`
-		} `yaml:"redis"`
-	} `yaml:"database"`
+	Server   Server   `yaml:"server"`
+	Database Database `yaml:"database"`
+	Logging  Logging  `yaml:"logging"`
+	Metrics  Metrics  `yaml:"metrics"`
+}
+
+type Server struct {
+	IPStart                string   `yaml:"ip_start"`
+	IPEnd                  string   `yaml:"ip_end"`
+	SubnetMask             string   `yaml:"subnet_mask"`
+	LeaseTime              int      `yaml:"lease_time"`
+	Gateway                string   `yaml:"gateway"`
+	ServerIP               string   `yaml:"server_ip"`
+	DNSServers             []string `yaml:"dns_servers"`
+	TFTPServerName         string   `yaml:"tftp_server_name"`
+	BootFileName           string   `yaml:"boot_file_name"`
+	Interface              string   `yaml:"interface"`
+	Port                   int      `yaml:"port" default:"67"`
+	CleanupExpiredInterval int      `yaml:"cleanup_expired_interval" default:"120"`
+	ARPCheck               bool     `yaml:"arp_check" default:"true"`
+}
+
+type Logging struct {
+	Level string `yaml:"level"`
+}
+
+type Metrics struct {
+	Enabled       bool   `yaml:"enabled" default:"true"`
+	ListenAddress string `yaml:"listen_address" default:":9100"`
+}
+
+type Database struct {
+	Type  string      `yaml:"type" default:"bolt"`
+	Bolt  BoltConfig  `yaml:"bolt"`
+	Redis RedisConfig `yaml:"redis"`
+}
+
+type BoltConfig struct {
+	Path string `yaml:"path"`
+}
+
+type RedisConfig struct {
+	Addr     string `yaml:"addr"`
+	Password string `yaml:"password"`
+	DB       int    `yaml:"db" default:"0"`
 }
 
 func LoadConfig(filePath string) (*Config, error) {
